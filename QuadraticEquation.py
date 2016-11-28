dd4e35af3b4749a60139563bfa2644ff19cbbbc7
tjel <tomek.jelinski@gmail.com>
2016-11-28 10:56:45 +0100

    Update QuadraticEquation.py

--- QuadraticEquation.py
+++ QuadraticEquation.py
@@ -7,12 +7,19 @@ d = b**2-4*a*c # discriminant
 if d < 0:
     print "This equation has no real solution"
 elif d == 0:
-    x = (-b + math.sqrt(b**2 - 4*a*c)) / (2 * a)
+    x = (-b) / (2 * a)
 
     print "This equation has one solutions: ", x
 else:
-    x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2 * a)
+    x1 = (-b - math.sqrt(d)) / (2 * a)
 
-    x2 = (-b + math.sqrt(b**2 - 4*a*c)) / (2 * a)
+    x2 = (-b + math.sqrt(d)) / (2 * a)
 
     print "This equation has two solutions: ", x1, " and", x2
+
+x ,y ,z = 2, 7, 3     
+delta = y**2-4*x*z
+X1 = (-y + math.sqrt(delta)) / (2 * x)
+X2 = (-y - math.sqrt(delta)) / (2 * x)
+
+print 'Test case: For x = {}, y = {}, z = {} solutions are: {} and {}'.format(x,y,z,X1,X2)
