4f464fe3ce011433d45ddabc5ec401a2336383cc
tjel <tomek.jelinski@gmail.com>
2016-12-02 19:26:33 +0100

    Update QuadraticEquation.py

--- QuadraticEquation.py
+++ QuadraticEquation.py
@@ -22,6 +22,3 @@ X1 = (-y + math.sqrt(delta)) / (2 * x)
 X2 = (-y - math.sqrt(delta)) / (2 * x)
 
 print 'Test case: For x = {}, y = {}, z = {} solutions are: {} and {}'.format(x,y,z,X1,X2)
-
-print 2x
-print(2x)
